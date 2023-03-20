1b388d8fa8c0b693ffa4d1ab8bffe1437284073f
Maicon Talgatti <dionestalgattimaicon@gmail.com>
2023-03-20 18:53:51 -0300

    a

--- teste.php
+++ teste.php
@@ -678,47 +678,11 @@
                 <input type="email" name="email" placeholder="Email@email.com" required><bR>
                 <label>Insira seu numero de celular (Whatsapp)</label><bR>
                 <input type="number" name="celular" placeholder="(ddd) 9999 9999" required>
-                <br>
-                <br>
+                <br> 
                 <input type="submit" value="enviar">
                 <br>
             </form>
-            <!-- <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-                        <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#">Disabled</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                            <div class="dropdown-menu" aria-labelledby="dropdown01">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                        </li>
-                        </ul>
-                        <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        </form>
-                    </div>
-                    </nav> -->
-
-            <!-- <input type="text" class="align_center"><br>
-        <input type="button" class="align_center" value="Iniciar" href="teste.php"> -->
-            <!-- <p class="lead align_center">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p> -->
+             
         </div>
 
 
