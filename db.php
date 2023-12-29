49bbb4852e667d17e52179dff3ec0dcc9a78801e
Maicon Talgatti <59876575+maicontalgatti@users.noreply.github.com>
2023-12-29 07:29:06 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
  <?php
 
 
-$user = "talgatti";
-$senha = "R[6UE2ZbEudMwlgP";
-$endereco = "sys.ampla.ind.br:3306";
-$banco = "automacaointerno";
+$user = "";
+$senha = "";
+$endereco = "";
+$banco = " ";
 
 $conn = mysqli_connect($endereco, $user, $senha, $banco);
 
